ee57e5a22b3ea13b27f05f3468d31de7c8007bae
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Pro-2.local>
2020-05-14 17:20:08 -0700

    Style changes

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -14,6 +14,35 @@ intervalVar = setInterval(isVSCodeOpen, 50)
 
 var db = firebase.firestore();
 
+var firebaseConfig = {
+    apiKey: "AIzaSyBmn_tDSlm4lLdrvSqj8Yb00KkYae8cL-Y",
+    authDomain: "neon-pulse-development.firebaseapp.com",
+    databaseURL: "https://neon-pulse-development.firebaseio.com",
+    projectId: "neon-pulse-development",
+    storageBucket: "neon-pulse-development.appspot.com",
+    messagingSenderId: "240091062123",
+    appId: "1:240091062123:web:babe11f5f03ced38fbb62e",
+    measurementId: "G-VMS6JL8H4S"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var logOutBtn = document.getElementById('logOutBtn');
+
+logOutBtn.addEventListener('click', function() {
+    firebase.auth().signOut().then(function() {
+        localStorage.removeItem('userid')
+        document.location.href = 'login.html'
+    }).catch(function(error) {
+        // Handle errors
+        dialog.showMessageBox({
+            type: 'error',
+            title: 'Error',
+            message: error.message
+        });
+        console.log(error);
+    })
+});
+
 async function isVSCodeOpen() {
     var list = await psList()
     var names = []
