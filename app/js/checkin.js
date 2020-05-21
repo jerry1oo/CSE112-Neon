9c1fa80cfb0ed68c7e62848df9d8b58246bdbf0c
Jiaxin Ge <jig099@ucsd.edu>
2020-05-20 18:24:24 -0700

    check prev tasks

--- app/js/checkin.js
+++ app/js/checkin.js
@@ -48,15 +48,58 @@ function checkTeams() {
             document.location.href = 'taskbar.html'
         });
 }
+function checkPrevTask(){
+    db.collection("teams").where(uid, "==", true)
+        .get()
+        .then(function(querySnapshot) {
+            console.log(querySnapshot.docs)
+            if (querySnapshot.docs.length > 0) {
+                querySnapshot.forEach(function(doc) {
+                    teamName = doc.id
+                });
+            }
+            return teamName
+
+        }).then(function(teamName){
+        db.collection("teams")
+        .doc(teamName)
+        .collection(uid).doc("status").get()
+        .then(function(status){
+           statusObj = status.data()
+           ptDiv= document.getElementById("prevTask")
+           if(statusObj.task1!="" || statusObj.task2!="" || statusObj.task3!=""){
+                ptDiv.style.display="block"
+           }
+           if(statusObj.task1!=""){
+               addTask(ptDiv,statusObj.task1)
+           }
+           if(statusObj.task2!=""){
+               addTask(ptDiv,statusObj.task2)
+           }
+           if(statusObj.task3!=""){
+               addTask(ptDiv,statusObj.task3)
+           }
+        })
+        .catch(function(error){
+            console.log("Error checking prev tasks",error)
+        });
+        });
+}
+checkPrevTask()
 
 var startFlowButton = document.getElementById("startFlowBtn")
 startFlowButton.addEventListener("click", () => startFlow())
 
+//startflow will always send 3 tasks value, if the user didn't not set any of them, just set the val to be ""
 function startFlow() {
-    var goalText = document.getElementById("goalText")
+    var task1 = document.getElementById("Task1")
+    var task2 = document.getElementById("Task2")
+    var task3 = document.getElementById("Task3")
     var obj = {
     	checkedIn: true,
-    	goal: goalText.value,
+        task1: task1.value,
+        task2: task2.value,
+        task3: task3.value,
     	taskStatus: 1
     }
     db.collection("teams").doc(teamName).collection(uid).doc("status").set(obj)
@@ -73,4 +116,22 @@ function startFlow() {
             console.error("Error adding document: ", error);
             document.location.href = 'taskbar.html'
         });
+}
+function addTask(parent,text){
+    let task = `
+    <li>
+        <div>
+            <input style="display: inline-block; ali" value = ${text}>
+            <button>Add</button>
+            <button>Delete</button>
+        </div>
+    </li>`
+    parent.insertAdjacentHTML('beforeend', task);
+}
+document.getElementById("prevTask").onclick=function(event){
+    let target = event.target;
+    parentNode = target.parentNode;
+    if(target.innerText=="Add"){
+        
+    }
 }
