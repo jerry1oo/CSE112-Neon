155d384881921ef14826a484e705c8dc5b751374
Chen Meng <c5meng@ucsd.edu>
2020-05-22 12:47:18 -0700

    Add comments and styles

--- app/js/taskbar.js
+++ app/js/taskbar.js
@@ -22,15 +22,12 @@ var status_emoji = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
+// User info
 var uid = localStorage.getItem('userid')
 var uname = localStorage.getItem('displayName')
 
-document.getElementById('username').innerHTML = uname;
-var nameObj = {}
-nameObj["displayName"] = uname;
-db.collection("users").doc(uid).update(nameObj);
-
-// Init user
+//Create user doc if not present in firebase, 
+//if the user is present, this will simply updates its status to online
 db.collection("users").doc(uid).update({ 
     "displayName": uname,
     "userStatus": 'Online'})
@@ -38,6 +35,8 @@ db.collection("users").doc(uid).update({
     console.error("Error adding document: ", error);
 });
 
+// Top user information logistics
+document.getElementById('username').innerHTML = uname;
 document.getElementById("userStatus").onchange = function(){
     var value = document.getElementById("userStatus").value;
     db.collection("users").doc(uid).update({
@@ -48,29 +47,31 @@ document.getElementById("userStatus").onchange = function(){
     });
  };
 
-function addStatusListener(id) {
-    db.collection("users").doc(id)
-    .onSnapshot(function(doc) {
-        var displayName = doc.get("displayName");
-        var status = doc.get("userStatus");
-        console.log(displayName + "change status to " + status);
-        onStatusChange(displayName, status)
-    });
-}
+// Top right log out button logistics
+var logoutButton = document.getElementById("logOutBtn")
+logoutButton.addEventListener("click", function() {
+    firebase.auth().signOut().then(function() {
+        localStorage.removeItem('userid')
+        localStorage.removeItem('email')
+        localStorage.removeItem('displayName')
+        document.location.href = 'signin.html'
+    }).catch(function(error) {
+        // Handle errors
+        dialog.showMessageBox({
+            type: 'error',
+            title: 'Error',
+            message: error.message
+        });
+        console.log(error);
+    })
+})
 
+// Left column logistics
 var startFlowButton = document.getElementById("startFlowButton")
 startFlowButton.addEventListener("click", () => startFlow())
 
-function startFlow() {
-    document.location.href = 'checkin.html'
-}
-
 var endFlowButton = document.getElementById("endFlowButton")
 endFlowButton.addEventListener("click", () => endFlow())
-
-function endFlow() {
-    document.location.href = 'checkout.html'
-}
 
 var flowDiv = document.getElementById("flowDiv")
 var teamDiv = document.getElementById("teamDiv")
@@ -78,12 +79,24 @@ flowDiv.style.display = "none"
 teamDiv.style.display = "none"
 endFlowButton.style.display = "none"
 
-var teamName = ""
+function startFlow() { document.location.href = 'checkin.html' }
+function endFlow() { document.location.href = 'checkout.html' }
+
+
+// Right column logistics
+var teamName;
+var createTeamButton = document.getElementById("createTeamButton")
+createTeamButton.addEventListener("click", () => createTeam())
+var joinTeamButton = document.getElementById("joinTeamButton")
+joinTeamButton.addEventListener("click", () => joinTeam())
+
 checkTeams()
 
+/* Check if the user is already in a team. 
+ * If so, join the team automatically
+ */
 function checkTeams() {
-    db.collection("teams").where(uid, "==", true)
-        .get()
+    db.collection("teams").where(uid, "==", true).get()
         .then(function(querySnapshot) {
             console.log(querySnapshot.docs)
             if (querySnapshot.docs.length > 0) {
@@ -114,6 +127,7 @@ function checkTeams() {
             document.location.href = 'signin.html'
         });
 }
+
 
 function getTeamStatus() {
     db.collection("users").where("team", "==", teamName).get() 
@@ -156,9 +170,6 @@ function checkStatus() {
         });
 }
 
-var createTeamButton = document.getElementById("createTeamButton")
-createTeamButton.addEventListener("click", () => createTeam())
-
 function createTeam() {
     console.log("Creating team")
     var teamDiv = document.getElementById("teamDiv")
@@ -189,9 +200,6 @@ function createTeam() {
     teamDiv.appendChild(inpElement)
     teamDiv.appendChild(buttonElement)
 }
-
-var joinTeamButton = document.getElementById("joinTeamButton")
-joinTeamButton.addEventListener("click", () => joinTeam())
 
 function joinTeam() {
     console.log("Join Team")
@@ -243,24 +251,12 @@ function joinTeam() {
     teamDiv.appendChild(buttonElement)
 }
 
-var logoutButton = document.getElementById("logOutBtn")
-logoutButton.addEventListener("click", function() {
-    firebase.auth().signOut().then(function() {
-        localStorage.removeItem('userid')
-        localStorage.removeItem('email')
-        localStorage.removeItem('displayName')
-        document.location.href = 'signin.html'
-    }).catch(function(error) {
-        // Handle errors
-        dialog.showMessageBox({
-            type: 'error',
-            title: 'Error',
-            message: error.message
-        });
-        console.log(error);
-    })
-})
+//Utility functions
 
+/* Adds the team member to the team div on UI
+ * name: user's name to display
+ * status: user's status, in string
+ */
 function addTeamMember(name, status){
     console.log("Adding member " + name + ", status: " + status);
     var init = false;
@@ -290,6 +286,23 @@ function addTeamMember(name, status){
     if(init) {teamDiv.appendChild(statuslist);}
 }
 
+/* Adds a listener to the status of the given user with id
+ * id: user's id
+ */ 
+function addStatusListener(id) {
+    db.collection("users").doc(id)
+    .onSnapshot(function(doc) {
+        var displayName = doc.get("displayName");
+        var status = doc.get("userStatus");
+        console.log(displayName + " change status to " + status);
+        onStatusChange(displayName, status)
+    });
+}
+
+/* Change the status of the team member on UI
+ * name: user's name
+ * status: user's new status
+ */
 function onStatusChange(name, status){
     var status_elem = document.getElementById("status_" + name);
     if(status_elem != null){
